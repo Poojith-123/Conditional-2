08bd368649a19f051970fa54d126e1abce8c9c0f
Poojith-123 <104519890+Poojith-123@users.noreply.github.com>
2022-04-27 22:30:11 +0530

    Add files via upload

---
+++ poojith_food.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+int main()
+{
+    int input;
+    scanf("%d", &input);
+    if (input == 1)
+    {
+        printf("Food item - Pizza \nPrice - Rs.179\n");
+    }
+    else if (input == 2)
+    {
+        printf("Food item - Burger \nPrice - Rs.129\n");
+    }
+    else if (input == 3)
+    {
+        printf("Food item - Pasta \nPrice - Rs.179\n");
+    }
+    else if (input == 4)
+    {
+        printf("Food item - French Fries \nPrice - Rs.99\n");
+    }
+    else if (input == 5)
+    {
+        printf("Food item - Sandwich \nPrice - Rs.149\n");
+    }
+}
